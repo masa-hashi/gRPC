dde46762d9dcb3aceb6b62c0d5c8771a954577b5
Masashi Hashimoto <mhashimo200@gmail.com>
2020-08-17 10:49:23 +0900

    Interceptor

--- hello-grcp/client/client.go
+++ hello-grcp/client/client.go
@@ -6,21 +6,16 @@ import (
 	"os"
 
 	pb "github.com/masa-hashi/hello-grpc"
-	"google.golang.org/grpc"
 	"google.golang.org/genproto/googleapis/rpc/errdetails"
+	"google.golang.org/grpc"
 	"google.golang.org/grpc/status"
-	"google.golang.org/grpc/credentials"
 )
 
-func main () {
+func main() {
 	addr := "localhost:50051"
-	cred, err := credentials.NewClientTLSFromFile("server.crt", "")
+	conn, err := grpc.Dial(addr, grpc.WithInsecure(), grpc.WithUnaryInterceptor(unaryInterceptor))
 	if err != nil {
-		log.Fatal(err)
-	}
-	conn, err := grpc.Dial(addr, grpc.WithTransportCredentials(cred))
-	if err != nil {
-		log.Fatalf("did not connct: %v", err)
+		log.Fatalf("did not connect: %v", err)
 	}
 	defer conn.Close()
 	c := pb.NewGreeterClient(conn)
@@ -37,8 +32,8 @@ func main () {
 			log.Printf("gRPC Error (message: %s)", s.Message())
 			for _, d := range s.Details() {
 				switch info := d.(type) {
-					case *errdetails.RetryInfo:
-						log.Printf(" RetryInfo: %v", info)
+				case *errdetails.RetryInfo:
+					log.Printf(" RetryInfo: %v", info)
 				}
 			}
 			os.Exit(1)
@@ -46,5 +41,15 @@ func main () {
 			log.Fatalf("could not greet: %v", err)
 		}
 	}
-	log.Printf("Greeting: %s" , r.Message)
+	log.Printf("Greeting: %s", r.Message)
+}
+
+func unaryInterceptor(ctx context.Context,
+	method string, req, reply interface{},
+	cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
+
+	log.Printf("before call: %s, request: %+v", method, req)
+	err := invoker(ctx, method, req, reply, cc, opts...)
+	log.Printf("after call: %s, response: %+v", method, reply)
+	return err
 }
